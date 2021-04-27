a64a39713bdfbd82a8e74233b04122069ca63be5
Vitaly Zaff <vitalyzavgorodnev@gmail.com>
2021-04-27 23:50:53 +0000

    Without Function

---
+++ without.js
@@ -0,0 +1,43 @@
+const eqArrays = function(arr1, arr2) {
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  } return true;
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2) === true) {
+    return console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    return console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
+
+
+const without = function(source, remove) {
+  let arr = [];
+  let notPresent = [];
+  for (let i = 0; i < source.length; i++) { 
+    arr[source[i]] = true;
+  }
+  for (let i = 0; i < remove.length; i++) {
+    if (arr[remove[i]]) {
+      delete arr[remove[i]];
+    } else {
+      arr[remove[i]] = true;
+    }
+  }
+  for (let j in arr) {
+    notPresent.push(j);
+  }
+  return console.log(notPresent);
+};
+
+
+without([1, 8, 9,], [9]);
+without([1, 2, 3], [1]);
+const words = ["hello", "world", "lighthouse"];
+without(words, ["lighthouse"]);
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);
+
