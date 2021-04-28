fbf64c2ccb336d2b328d973ed7fa96e0b6428154
Vitaly Zaff <vitalyzavgorodnev@gmail.com>
2021-04-28 21:12:33 +0000

    Find key by value function

---
+++ findKeyByValue.js
@@ -0,0 +1,28 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    return console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    return console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+const bestTVShowsByGenre = {
+  // eslint-disable-next-line camelcase
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+const findKeyByValue = function(object, value) {
+  let keys = Object.keys(object);
+  for (let key of keys) {
+    if (object[key] === value) {
+      return key;
+    }
+  }
+};
+
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
