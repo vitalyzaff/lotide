23e64ae0759dfa2f2268ba77937b0398205e3daf
Vitaly Zaff <vitalyzavgorodnev@gmail.com>
2021-04-28 23:21:18 +0000

    Assertion of Objects

---
+++ assertObjectsEqual.js
@@ -0,0 +1,35 @@
+const eqArrays = function(arr1, arr2) {
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  } return true;
+};
+
+
+const eqObjects = function(obj1, obj2) {
+  const objKeys1 = Object.keys(obj1);
+  const objKeys2 = Object.keys(obj2);
+  if (objKeys1.length !== objKeys2.length) {
+    return false;
+  } else {
+    for (const key of objKeys1) {
+      if (Array.isArray(objKeys1[key]) && Array.isArray(objKeys2[key])) {
+        return eqArrays(obj1[key], obj2[key]);
+      } else if (obj1[key].length !== obj2[key].length) {
+        return false;
+      }
+    }
+  }
+  return true;
+};
+
+
+const assertObjectsEqual = function(actual, expected) {
+  const inspect = require('util').inspect;
+  if (eqObjects(actual, expected) === true) {
+    return console.log(`✅✅✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
+  } else {
+    return console.log(`🛑🛑🛑 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
+  }
+};
